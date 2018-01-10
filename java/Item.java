0a63b7f6666c66a5240fbc2653090dd17584af4f
PaulYeh813 <31879980+PaulYeh813@users.noreply.github.com>
2018-01-10 19:35:49 +0800

    Create Item.java

---
+++ java/Item.java
@@ -0,0 +1,20 @@
+public class Item{
+    String text;
+    boolean check;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isCheck() {
+        return check;
+    }
+
+    public void setCheck(boolean check) {
+        this.check = check;
+    }
+}
